62232ab8c5ad76ac886814be19dc171ffca36799
Abdu <abdturkmen@hotmail.com>
2023-03-17 18:09:12 +0300

    some changes made

--- hw1/main.c
+++ hw1/main.c
@@ -2,8 +2,8 @@
 #include "util.h"
 
 int main(){
- printf("GCD is = %d\n\n",part1()); /*prints output of GDC function*/
 
+ printf("GCD is = %d\n\n",part1()); /*prints output of GDC function*/
  /*calls other parts of homework*/
  part2();
  part3();
