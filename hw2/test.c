1b0b7d09a4a8ce4e1efc9c73648a4a9612dacb58
Abdullah TÜRKMEN <aturkmen2021@gtu.edu.tr>
2023-03-23 01:03:20 +0300

    a

--- hw2/test.c
+++ hw2/test.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
+#include <limits.h>
 
-int main(){
-    int a=2;
-    int b=3;
-    float c=3;
-
-    printf("%*.*f",a,b,c);
-
+int main() {
+    double myDouble = 3.14159265359;
+    int numDigits = 0;
+    int myInt;
+    while (1) {
+        numDigits++;
+        int factor = 1;
+        for (int i = 0; i < numDigits; i++) {
+            factor *= 10;
+        }
+        if (myDouble >= (double)INT_MAX / factor) {
+            factor = (int)(LONG_MAX / myDouble);
+        }
+        myInt = (int)(myDouble * factor);
+        if (myInt % factor == 0) {
+            break; // If the integer value is precise, exit the loop
+        }
+    }
+    printf("Original double: %f\n", myDouble);
+    printf("Integer with all decimal digits: %d\n", myInt);
+    return 0;
 }
