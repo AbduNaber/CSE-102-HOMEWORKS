e5ed6780707822b656bc63e1494ee21713354130
Abdullah TÜRKMEN <aturkmen2021@gtu.edu.tr>
2023-03-23 17:14:13 +0300

    part2

--- hw2/main.c
+++ hw2/main.c
@@ -57,13 +57,41 @@ double calculation(char operator, double operand1, double operand2){
     return result;
 }
 
+int digit_counter(int a){
+    
+    int counter = 0; /*to count digit*/
+    
+    /*increase digit counter until given number has no digit*/
+    while(a !=0){
+        counter++; /* increase digit counter */
+
+        /*reduce one digit of given number*/
+        a = a - (a%10);
+        a = a/10;
+    }
+
+    return counter;
+}
+
+
 void display(char format, int m, int n,double result){
+
+    int result_int;
+    double result_float;
+    int digit_of_int ;
+    int e=0;
     if(format=='S' || format =='s'){
-        int result_int;
-        double result_float;
+        
         result_int = (int)result;
         result_float = result - result_int;
-
+        digit_of_int = digit_counter(result_int);
+        if ((m-n)>digit_of_int){
+            e++;
+            result_int = result_int *10;
+            result_int = result_int + (int)(result_float*10);
+            digit_of_int++;
+        }
+        
     }
     else if(format=='I' || format =='i'){
 
