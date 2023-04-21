3568050c296d8a362e31993bba5be3ff81e4aaf6
Abdullah TÜRKMEN <aturkmen2021@gtu.edu.tr>
2023-04-21 23:49:35 +0300

    hw5 is done

--- hw5/main.c
+++ hw5/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <math.h>
 
-#define MAX_CHAR_LENGHT 255
-#define TICTACTOE_SIZE 3
+#define MAX_CHAR_LENGHT 255 // MAX FILE NAME LENGHT IN LINUX
+#define TICTACTOE_SIZE 3    // TIK TAC TOE GAME BOARD SIZE
 
+/*Function Declaration. For more info find out that function */
 void print_board(char tictactoe[TICTACTOE_SIZE][TICTACTOE_SIZE]);
 void create_board(char tictactoe[TICTACTOE_SIZE][TICTACTOE_SIZE]);
 int update_board(int x,int y,int player, char tictactoe[TICTACTOE_SIZE][TICTACTOE_SIZE]);
+
+
 
 typedef enum {
     RED , 
@@ -16,53 +19,57 @@ typedef enum {
     ORANGE,
 } Color;
 
-
 Color mixer(Color color1, Color color2,float arr[6][3]);
 Color colorMixer(Color color1, Color color2, float arr[6][3], Color (*func)(Color, Color, float (*)[3]));
-
 void print_color(Color color,float arr[6][3]);
+/*End  of Declaration*/
 
-
+//reads  a text  file  and  counts  the  occurrences  of  each  letter  in  the  English alphabet (case-insensitive)
 int part1(char filename[]){
 
-    FILE *fp= fopen(filename,"r");
+    FILE *fp= fopen(filename,"r");// open file in read mode
 
     if(fp == NULL){
-      printf("Error: Could not open file\n");
+      printf("Error: Could not open file\n\n");
       return 1;
     }
-    int alphabet[26] = {0};
+
+    int alphabet[26] = {0};// Array stores how many repeated char does it have. a is alphabet [0] so z is alphabet[25]
 
     char c;
     int locationLower= 'a';  // set at the begining of the alphabet 'a' 
     int locationUpper = 'A' ;  // set at the begining of the alphabet 'A' 
 
+    /* increase alphabet array corresponding to char.(case-insensitive) */
     while((c=fgetc(fp))!=EOF){
         if(c>='a' && c<='z'){
-            alphabet[c-locationLower]++;
+            alphabet[c-locationLower]++; 
         }
         else if(c>='A' && c<='Z'){
             alphabet[c-locationUpper]++;
         }
     }
 
+    /* display letter frequency*/
     printf("Letter Frequency:\n");
     for(int i=0;i<26;i++){
         printf("%c: %d\n",i+'A',alphabet[i]);
     }
-    fclose(fp);
+    printf("\n");
+    fclose(fp);//close file
 }
 
-
+/*COLOR SECTION PART*/
 int part2(){
-
-    float color_vector[6][3] = {
+    
+    /*Color vector declaration, Each color has own point.*/
+    float color_vector[6][3] = { 
         {1.0,0.0,0.0}, // RED
         {0.0,1.0,0.0}, // GREEN
         {0.0,0.0,1.0}, // BLUE
         {0.5,0.5,0.0}, // YELLOW
         {0.5,0.4,0.2}, // ORANGE
-        {0.0,0.0,0.0}, // NEW COLOR 
+        {0.0,0.0,0.0}, // NEW COLOR (Choosen from user it occurs mixer function)
     } ;
     
 
@@ -71,10 +78,7 @@ int part2(){
     Color color1;
     Color color2;
     
-
-
-
-
+    /*COLOR SELECTION*/
     int flag=0;
     do{
         printf("Enter Color 1 (r,g,b,y,o): ");
@@ -109,8 +113,11 @@ int part2(){
              break;
         }
     }while(!flag);
+    /*END OF SELECTION*/
 
-    Color new_color = colorMixer(color1,color2,color_vector,&mixer);
+    Color new_color = colorMixer(color1,color2,color_vector,&mixer);// mix the color. see colorMixer function
+
+    /*display the mixed color*/
     printf("Mixed Color: ");
     switch(new_color){
         case RED: printf("RED "); print_color(new_color,color_vector); break;  
@@ -118,35 +125,44 @@ int part2(){
         case BLUE: printf("BLUE "); print_color(new_color,color_vector); break; 
         case YELLOW: printf("YELLOW"); print_color(new_color,color_vector); break; 
         case ORANGE: printf("ORANGE"); print_color(new_color,color_vector); break;            
-    }   
+    } 
+    /*end  of display*/  
 }
 
-
+/*
+ *transition function to mixer
+ *it takes Color,float array and pointer function as argument
+ */
 Color colorMixer(Color color1, Color color2, float arr[6][3], Color (*func)(Color, Color, float (*)[3] )){
     Color new_color = mixer(color1,color2,arr);
     return new_color;
 
-
 }
 
-
+/*                              mixer 
+  *When the colors to be mixed has the followinglength-3 vectors [x1,x2,x3] and [y1,y2,y3], 
+  *the mixed color is [(x1+y1)/2, (x2+y2)/2, (x3+yr)/2] 
+  *the corresponding enumerated color is the closest to this one using Euclidean distance  
+*/
 Color mixer(Color color1, Color color2,float arr[6][3]){
 
-    float distances[5]= {0.0,0.0,0.0,0.0,0.0};
-      
-    Color final_color; 
+    float distances[5]= {0.0,0.0,0.0,0.0,0.0}; // distances to each color (RED to ORANGE)
+    Color final_color;
 
-
+    /*calculation of new color*/
     for(int i=0;i<3;i++){
         arr[5][i]= (arr[color1][i]+arr[color2][i])/2;
     }
 
+    /*calculation of distances to each color */
     for(Color i=RED;i<= ORANGE;i++){
         distances[i]= sqrt(pow(arr[5][0] - arr[i][0], 2) + pow(arr[5][1] - arr[i][1], 2) + pow(arr[5][2]-  arr[i][2],2));
 
     }
 
-    float min_distance = distances[0];
+    float min_distance = distances[0];// set to distance to zero for determining min distance
+    
+    // determine Which color is closest to
     for(Color i=RED;i<= ORANGE;i++){
 
         if(min_distance > distances[i]){
@@ -158,19 +174,23 @@ Color mixer(Color color1, Color color2,float arr[6][3]){
     return final_color;
 
 }
-
+/* print the mixed color vector*/
 void print_color(Color color,float arr[6][3]){
-    printf("[%.2f, %.2f, %.2f]\n",arr[5][0],arr[5][1],arr[5][2]);
+    printf("[%.2f, %.2f, %.2f]\n\n",arr[5][0],arr[5][1],arr[5][2]);
 }
 
 
+// CLASSIC TIC TAC TOE GAME
 int part3(){
+
     char tictactoe[TICTACTOE_SIZE][TICTACTOE_SIZE] ;
-    int x,y;
-    int status=0;
-    int movec=0;
-    int player;
-    create_board(tictactoe);
+    int x,y; // user chosen location parameter 
+    int status=0;// check win or resume game if it is 88 ('X') X is winner or if it is 79 ('O') O is winner
+    int movec=0; // caunt movement of game, if it reachs 9, it stops the game 
+
+    create_board(tictactoe);// creates empty board
+
+    /*PLAYGROUND*/
     while( status != 'X'&& status != 'O'&& movec!=9 ){
 
             printf("player 1 (X), Enter your  move (row,col): ");
@@ -188,9 +208,8 @@ int part3(){
                  movec++;
             }
           
-           
-        
-    }
+            }
+    /*END OF PLAYGROUND*/
 
     if(status=='X'){
         printf("Player 1 (X) wins!\n");
@@ -201,15 +220,17 @@ int part3(){
     else{
         printf("NO WINNER, NO LOSER\n");
     }
-    
-                        
-
+    printf("END OF GAME GOOD BYE\n");
 }
 
+// Updates board according to user chosen X and Y location
+// Additional Checks potential winner and return winner
 int update_board(int x,int y,int player, char tictactoe[TICTACTOE_SIZE][TICTACTOE_SIZE]){
 
     int status =0;
     int flag=0;
+
+    //set the movement to board. If it is full, warn user
     while(!flag){
 
         if( tictactoe[x][y] == '_'){
@@ -229,17 +250,17 @@ int update_board(int x,int y,int player, char tictactoe[TICTACTOE_SIZE][TICTACTO
         }
     }
     
-
-
-
+    // check potential winner in row.
     if(tictactoe[x][0]==tictactoe[x][1]&& tictactoe[x][1]== tictactoe[x][2]){
         status = tictactoe[x][y];
     }
 
+    // check potential winner in column.
     else if (tictactoe[0][y]==tictactoe[1][y]&& tictactoe[1][y]== tictactoe[2][y] ){
         status = tictactoe[x][y];
     }
 
+    // check potential winner in cross.
     else if((x==0 && y==0) || (x==1 && y==1) ||(x==2 && y==2) ){
         if((tictactoe[0][0]==tictactoe[1][1]) && (tictactoe[1][1]==tictactoe[2][2])){
             status = tictactoe[x][y];
@@ -247,8 +268,10 @@ int update_board(int x,int y,int player, char tictactoe[TICTACTOE_SIZE][TICTACTO
     } 
 
      return status;
+
     }
 
+// CREATE BOARD
 void create_board(char tictactoe[TICTACTOE_SIZE][TICTACTOE_SIZE]){
     for (int i = 0; i < 3; i++){
          for (int j = 0; j < 3; j++){
@@ -257,6 +280,7 @@ void create_board(char tictactoe[TICTACTOE_SIZE][TICTACTOE_SIZE]){
     }
 }
 
+// PRINT BOARD
 void print_board(char tictactoe[TICTACTOE_SIZE][TICTACTOE_SIZE]){
 
      for (int i = 0; i < 3; i++){
@@ -265,22 +289,22 @@ void print_board(char tictactoe[TICTACTOE_SIZE][TICTACTOE_SIZE]){
          }
          printf("\n");
     }
-
-
-
-
 }
+
 
 int main(){
 
-    char filename[MAX_CHAR_LENGHT] ;
-    printf("Enter the file name: ");
+    char filename[MAX_CHAR_LENGHT] ; // store file name
+    //prompts user to file name and send to part1 function
+    printf("Enter the file name: "); 
     scanf("%s",filename);
-    part1(filename); 
-    part2();
-    part3();
+    part1(filename);
 
-    
+    part2(); // COLOR MIXER
+    part3();  // TICTACTOE GAME
+
+    return 0;
+
 }
 
 
