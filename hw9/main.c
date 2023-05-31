887ad0874247848febc7ee8546fea5ba88b4f4a7
Abdullah TÜRKMEN <aturkmen2021@gtu.edu.tr>
2023-05-31 10:41:58 +0300

    no masenger

--- hw9/main.c
+++ hw9/main.c
@@ -2,20 +2,12 @@
 #include "util.h"
 
 int main(){
-<<<<<<< HEAD
-    block_t ***board =init_board(); ;
-   
-    play(board);   
-     
-   
-=======
     
     block_t ***board;
     init_board(board);
     printf("%d",board[0][0][0].type);
 
    // draw_board(board,snake,2);
->>>>>>> ac3247a0f6e7da976a9306ae6cdb552715b0caaa
 
     // free board
    for(int i=0;i<ROW;i++){
